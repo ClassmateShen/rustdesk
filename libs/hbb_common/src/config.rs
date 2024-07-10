07506cc4287bec9f4476333166706451b2b21f39
叫我沈同学 <97521809+ClassmateShen@users.noreply.github.com>
2024-07-10 16:48:10 +0800

    Update pub

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["remote-desk.shenkx.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "NWbA+wRukp7lp3+bJ0PfiNYealL6PKLUOo6vQH6oPKM=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
